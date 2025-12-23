66f3768aff298659e74d1e77f1ad56764e0dd4b7
itsnhyyy <22521038@gm.uit.edu.vn>
2025-12-23 19:14:07 +0700

    Setup API for auth, news

--- frontend/src/features/users/routers/Auth/index.tsx
+++ frontend/src/features/users/routers/Auth/index.tsx
@@ -1,8 +1,6 @@
 import Login from '../../pages/Auth/Login';
 import Register from '../../pages/Auth/Register';
 import ForgotPassword from '../../pages/Auth/ForgotPassword';
-import ResetPassword from '../../pages/Auth/ResetPassword';
-import VerifyOtp from '../../pages/Auth/VerifyOTP';
 
 const AuthRouters = [
   {
@@ -17,14 +15,6 @@ const AuthRouters = [
     path: '/users/forgot-password',
     element: <ForgotPassword />,
   },
-  {
-    path: '/users/reset-password/:token',
-    element: <ResetPassword />,
-  },
-  {
-    path: '/users/verify-otp',
-    element: <VerifyOtp />,
-  }
 ];
 
 export default AuthRouters;
